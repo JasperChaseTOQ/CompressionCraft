7c32cc1e05b7c7f5f1d29ed51ad3405f5387dc58
JasperChaseTOQ <103849596+JasperChaseTOQ@users.noreply.github.com>
2022-06-08 18:56:38 -0400

    update

--- src/main/java/me/jasperchasetoq/condensationcraft/CondensationCraft.java
+++ src/main/java/me/jasperchasetoq/condensationcraft/CondensationCraft.java
@@ -1,4 +1,4 @@
-package me.CHANGEME.slimefunaddon;
+package me.jasperchasetoq.condensationcraft;
 
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
@@ -13,7 +13,7 @@ import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 
-public class ExampleAddon extends JavaPlugin implements SlimefunAddon {
+public class CondensationCraft extends JavaPlugin implements SlimefunAddon {
 
     @Override
     public void onEnable() {
